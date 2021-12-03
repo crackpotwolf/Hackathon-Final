786e41fde688c49f9c351e44eb7f8cb40a2010e1
Vladimir Artyushin <38529023+crackpotwolf@users.noreply.github.com>
2021-12-03 22:54:47 +0300

    Тепловая карта

---
+++ frontend/accelerator/src/app/pages/heatmap/datamap.ts
@@ -0,0 +1,201 @@
+export const jsonmap =[
+  {
+    "TechnologicalDirection": "Доступный и комфортный городской транспорт",
+    "Cells": {
+      "geoData": {"type": "Point", "coordinates": [55.73206408441163,37.667501003968276]}
+    }
+  },
+  {
+    "TechnologicalDirection": "Доступный и комфортный городской транспорт",
+    "Cells": {
+      "geoData": {"type": "Point", "coordinates": [55.73054925078848,37.66060381538676]}
+    }
+  },
+  {
+    "TechnologicalDirection": "Доступный и комфортный городской транспорт",
+    "Cells": {
+      "geoData": {"type": "Point", "coordinates": [55.72489425278286,37.6635113299589]}
+    }
+  },
+  {
+    "TechnologicalDirection": "Доступный и комфортный городской транспорт",
+    "Cells": {
+      "geoData": {"type": "Point", "coordinates": [55.739543439909966,37.65345570759624]}
+    }
+  },
+  {
+    "TechnologicalDirection": "Доступный и комфортный городской транспорт",
+    "Cells": {
+      "geoData": {"type": "Point", "coordinates": [55.74181656616614,37.654707299263386]}
+    }
+  },
+  {
+    "TechnologicalDirection": "Доступный и комфортный городской транспорт",
+    "Cells": {
+      "geoData": {"type": "Point", "coordinates": [55.74699708078098,37.65580164054146]}
+    }
+  },
+  {
+    "TechnologicalDirection": "Новые виды мобильности",
+    "Cells": {
+      "geoData": {"type": "Point", "coordinates": [55.79253373284079,37.63607664642309]}
+    }
+  },
+  {
+    "TechnologicalDirection": "Новые виды мобильности",
+    "Cells": {
+      "geoData": {"type": "Point", "coordinates": [55.800197407374576,37.63573332366917]}
+    }
+  },
+  {
+    "TechnologicalDirection": "Новые виды мобильности",
+    "Cells": {
+      "geoData": {"type": "Point", "coordinates": [55.79326262420893,37.61619191380135]}
+    }
+  },
+  {
+    "TechnologicalDirection": "Новые виды мобильности",
+    "Cells": {
+      "geoData": {"type": "Point", "coordinates": [55.781413808325624,37.61438946934334]}
+    }
+  },
+  {
+    "TechnologicalDirection": "Новые виды мобильности",
+    "Cells": {
+      "geoData": {"type": "Point", "coordinates": [55.779382219336235,37.60084967823616]}
+    }
+  },
+  {
+    "TechnologicalDirection": "Новые виды мобильности",
+    "Cells": {
+      "geoData": {"type": "Point", "coordinates": [55.77680699115155,37.585356933194014]}
+    }
+  },
+  {
+    "TechnologicalDirection": "Безопасность дорожного движения",
+    "Cells": {
+      "geoData": {"type": "Point", "coordinates": [55.72886392630508,37.622791700493394]}
+    }
+  },
+  {
+    "TechnologicalDirection": "Безопасность дорожного движения",
+    "Cells": {
+      "geoData": {"type": "Point", "coordinates": [55.729862893406484,37.62569921506554]}
+    }
+  },
+  {
+    "TechnologicalDirection": "Безопасность дорожного движения",
+    "Cells": {
+      "geoData": {"type": "Point", "coordinates": [55.72902134100666,37.616322212349466]}
+    }
+  },
+  {
+    "TechnologicalDirection": "Безопасность дорожного движения",
+    "Cells": {
+      "geoData": {"type": "Point", "coordinates": [55.72759853112582,37.61471288694053]}
+    }
+  },
+  {
+    "TechnologicalDirection": "Безопасность дорожного движения",
+    "Cells": {
+      "geoData": {"type": "Point", "coordinates": [55.72757431263459,37.609734707008904]}
+    }
+  },
+  {
+    "TechnologicalDirection": "Безопасность дорожного движения",
+    "Cells": {
+      "geoData": {"type": "Point", "coordinates": [55.723553833973206,37.61082904828697]}
+    }
+  },
+  {
+    "TechnologicalDirection": "Здоровые улицы и экология",
+    "Cells": {
+      "geoData": {"type": "Point", "coordinates": [55.66328379628663,37.6316533283999]}
+    }
+  },
+  {
+    "TechnologicalDirection": "Здоровые улицы и экология",
+    "Cells": {
+      "geoData": {"type": "Point", "coordinates": [55.66378109302827,37.62006618545558]}
+    }
+  },
+  {
+    "TechnologicalDirection": "Здоровые улицы и экология",
+    "Cells": {
+      "geoData": {"type": "Point", "coordinates": [55.668511249612486,37.590025444488774]}
+    }
+  },
+  {
+    "TechnologicalDirection": "Здоровые улицы и экология",
+    "Cells": {
+      "geoData": {"type": "Point", "coordinates": [55.679606459402216,37.595990677337895]}
+    }
+  },
+  {
+    "TechnologicalDirection": "Здоровые улицы и экология",
+    "Cells": {
+      "geoData": {"type": "Point", "coordinates": [55.68821126048799,37.57320905390388]}
+    }
+  },
+  {
+    "TechnologicalDirection": "Здоровые улицы и экология",
+    "Cells": {
+      "geoData": {"type": "Point", "coordinates": [55.69202934791753,37.56202960672982]}
+    }
+  },
+  {
+    "TechnologicalDirection": "Здоровые улицы и экология",
+    "Cells": {
+      "geoData": {"type": "Point", "coordinates": [55.68407760725155,37.551944500833834]}
+    }
+  },
+
+  {
+    "TechnologicalDirection": "Цифровые технологии в транспорте",
+    "Cells": {
+      "geoData": {"type": "Point", "coordinates": [55.74941277647942,37.531385422083794]}
+    }
+  },
+  {
+    "TechnologicalDirection": "Цифровые технологии в транспорте",
+    "Cells": {
+      "geoData": {"type": "Point", "coordinates": [55.75358789617277,37.53127813372321]}
+    }
+  },
+  {
+    "TechnologicalDirection": "Цифровые технологии в транспорте",
+    "Cells": {
+      "geoData": {"type": "Point", "coordinates": [55.75592334710148,37.53893852266974]}
+    }
+  },
+  {
+    "TechnologicalDirection": "Цифровые технологии в транспорте",
+    "Cells": {
+      "geoData": {"type": "Point", "coordinates": [55.766183146251265,37.538294792506164]}
+    }
+  },
+  {
+    "TechnologicalDirection": "Цифровые технологии в транспорте",
+    "Cells": {
+      "geoData": {"type": "Point", "coordinates": [55.76905008185538,37.51544237169929]}
+    }
+  },
+  {
+    "TechnologicalDirection": "Цифровые технологии в транспорте",
+    "Cells": {
+      "geoData": {"type": "Point", "coordinates": [55.77789877121088,37.52273089557431]}
+    }
+  },
+  {
+    "TechnologicalDirection": "Цифровые технологии в транспорте",
+    "Cells": {
+      "geoData": {"type": "Point", "coordinates": [55.77628059331549,37.50320868922881]}
+    }
+  },
+  {
+    "TechnologicalDirection": "Цифровые технологии в транспорте",
+    "Cells": {
+      "geoData": {"type": "Point", "coordinates": [55.7722046760848,37.49535518123321]}
+    }
+  }
+];
